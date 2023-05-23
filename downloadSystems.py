1744ac5eb2424f89ad36434ed9a7b353573dbfc4
Fschatbot <s.himanshu.sultania@fountainheadschools.org>
2023-05-23 19:22:22 +0530

    Fixed a small bug

--- downloadSystems.py
+++ downloadSystems.py
@@ -1,7 +1,7 @@
 import requests, json
 from rich.progress import Progress
 
-
+progressiveDump = False # Set to false for speed, true for quick output
 systems = []
 counter = 1
 
@@ -12,7 +12,9 @@ with Progress() as progress:
 		response = request.json()
 		progress._tasks[task].total = response['meta']['total']
 		progress.advance(task, len(response['data']))
+		systems.extend(response['data'])
 		counter += 1
+		if progressiveDump: json.dump(systems, open('public/systems.json', 'w'))
 
 with open('public/systems.json', 'w') as f:
 	json.dump(systems, f)
