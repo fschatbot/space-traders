1fcb572f40c663decdb7b989c970b65a06435db3
Fschatbot <s.himanshu.sultania@fountainheadschools.org>
2023-05-24 12:04:26 +0530

    Made the downloader slightly more better regarding limitations

--- downloadSystems.py
+++ downloadSystems.py
@@ -1,19 +1,30 @@
 import requests, json
+from time import sleep
 from rich.progress import Progress
 
-progressiveDump = False # Set to false for speed, true for quick output
+progressiveDump = True # Set to false for speed, true for quick output
 systems = []
 counter = 1
 
 with Progress() as progress:
 	task = progress.add_task("[cyan]Downloading system data...", total=1)
 	while not progress.finished:
-		request = requests.get(f"https://api.spacetraders.io/v2/systems?limit=20&page={counter}")
+		try:
+			request = requests.get(f"https://api.spacetraders.io/v2/systems?limit=20&page={counter}")
+		except:
+			print("Error: Connection error")
+			sleep(5)
+			continue
 		response = request.json()
+		if response.get('error'):
+			print(f"Error: {response['error']['message']}")
+			sleep(1)
+			continue
 		progress._tasks[task].total = response['meta']['total']
 		progress.advance(task, len(response['data']))
 		systems.extend(response['data'])
 		counter += 1
+		sleep(0.4)
 		if progressiveDump: json.dump(systems, open('public/systems.json', 'w'))
 
 with open('public/systems.json', 'w') as f:
