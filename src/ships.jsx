9b3355370b3c1cf15cc7761685ec045b7640e595
Fschatbot <s.himanshu.sultania@fountainheadschools.org>
2023-05-26 15:19:21 +0530

    Completed Mining. Better regex. File Name shift.

--- src/ships.jsx
+++ src/ships.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useReducer, useRef, useState } from "react";
+import { useCallback, useContext, useEffect, useReducer, useRef, useState } from "react";
 import { CallEndPoint, DataProvider, ENDPOINTS } from "./spaceAPI";
 import { BiInfoCircle, BiRocket } from "react-icons/bi";
 import { BsEjectFill } from "react-icons/bs";
@@ -6,10 +6,11 @@ import { BiCurrentLocation } from "react-icons/bi";
 import { HiOutlineLocationMarker, HiChevronDown, HiOutlineRefresh } from "react-icons/hi";
 import "./styles/ships.css";
 import { toast } from "react-toastify";
+import { useCountDown } from "./utils";
 
 export default function Ships() {
 	const { store, updateStore } = useContext(DataProvider);
-	useEffect(refresh, []);
+	// useEffect(refresh, []);
 
 	function refresh() {
 		CallEndPoint({ endpoint: ENDPOINTS.AGENT_SHIPS, remember: 5 * 60 * 1000 })
@@ -48,7 +49,7 @@ function ShowShip({ data }) {
 		NavigationActionRef.current.classList.add("pending");
 		NavigationActionRef.current.classList.remove("error");
 		// Basic verfication for quick response and decreased API call
-		if (NavigationRef.current.value.length !== 14 || /^[A-z0-9]{2}-[A-z0-9]{4}-\d{5}[A-z0-9]$/g.test(NavigationRef.current.value) === false) {
+		if (/^X1-\w{1,2}\d{1,2}-\d{5}\w$/g.test(NavigationRef.current.value) === false) {
 			NavigationActionRef.current.classList.remove("pending");
 			NavigationActionRef.current.classList.add("error");
 			toast.error("Invalid route format");
@@ -102,7 +103,7 @@ function ShowShip({ data }) {
 						<BiCurrentLocation />
 						{data.nav.status !== "IN_TRANSIT"
 							? `${data.nav.route.destination.symbol} (${data.nav.route.destination.x}, ${data.nav.route.destination.y}) [${data.nav.route.destination.type.replaceAll("_", " ").title()}]`
-							: `The ship will arive at a location at ${new Date(data.nav.route.arrival).preset("DD/MM/YYYY HH:mm:ss")}`}
+							: `The ship will arive at a location at ${new Date(data.nav.route.arrival).preset("DD/MM/YYYY hh:mm:ss")}`}
 					</span>
 					<span>
 						<HiOutlineLocationMarker />
@@ -111,7 +112,7 @@ function ShowShip({ data }) {
 							: `${data.nav.route.destination.symbol} (${data.nav.route.destination.x}, ${data.nav.route.destination.y}) [${data.nav.route.destination.type.replaceAll("_", " ").title()}]`}
 					</span>
 					<div className="navigateAction" ref={NavigationActionRef}>
-						<input type="text" placeholder="XX-XXXX-00000X" maxLength={14} ref={NavigationRef} onKeyUp={(e) => (e.key === "Enter" ? NavigateShip() : 0)} />
+						<input type="text" placeholder="XX-XX00-00000X" maxLength={14} ref={NavigationRef} onKeyUp={(e) => (e.key === "Enter" ? NavigateShip() : 0)} />
 						<button onClick={NavigateShip}>
 							<BiRocket />
 						</button>
@@ -153,6 +154,49 @@ function ActionButtons({ data, refresh }) {
 	const MODES = ["CRUISE", "BURN", "DRIFT", "STEALTH"];
 	delete MODES[MODES.indexOf(data.nav.flightMode)];
 
+	function Mine(survey) {
+		CallEndPoint({ endpoint: ENDPOINTS.EXTRACT, params: { ship: data.symbol }, body: survey ? { survey } : {} })
+			.then((res) => {
+				toast.success(`Mined ${res.data.extraction.yield.units} ${res.data.extraction.yield.symbol.replace("_", " ").title()}`);
+				setChaseTime(res.data.cooldown.expiration);
+			})
+			.then(refresh)
+			.catch(MineErr);
+	}
+	function SurveyMine() {
+		CallEndPoint({ endpoint: ENDPOINTS.SURVEY, params: { ship: data.symbol } })
+			// .then((response) => response.data.surveys[0].signature)
+			.then((response) => {
+				let survey = response.data.surveys[0]; // Getting the signature of the survey (TODO: Check out which survey to choose)
+				setChaseTime(response.data.cooldown.expiration);
+				onTimerEnd(() => Mine(survey));
+			})
+			.then(() => toast.success("Survey Complete"))
+			.catch(MineErr);
+	}
+
+	function MineErr(err) {
+		if (err.error) {
+			if (err.error.code === 4000) {
+				toast.error("The ship was on cooldown");
+				setChaseTime(err.error.data.cooldown.expiration);
+			} else {
+				toast.error(err.error.message);
+			}
+		} else {
+			console.warn(err);
+		}
+	}
+
+	const { timeLeft, setChaseTime, onTimerEnd } = useCountDown();
+
+	const MineableLocation = data.nav.route.destination.type === "ASTEROID_FIELD";
+	const CanMine = data.mounts.find((mount) => mount.symbol.includes("MOUNT_MINING_LASER")) !== undefined;
+	const CanSurvery = data.mounts.find((mount) => mount.symbol.includes("MOUNT_SURVEYOR")) !== undefined;
+	const CanRefine = data.modules.find((mount) => mount.symbol.includes("MODULE_MINERAL_PROCESSOR")) !== undefined; // Doubtful
+	const FullCargo = data.cargo.units === data.cargo.capacity;
+	const isInMineState = data.nav.status !== "IN_ORBIT" || FullCargo || timeLeft.total_seconds > 0;
+
 	return (
 		<div className="actionGroup">
 			<button onClick={ToggleOrbit} disabled={data.nav.status === "IN_TRANSIT"}>
@@ -185,10 +229,18 @@ function ActionButtons({ data, refresh }) {
 					))}
 				</div>
 			</div>
-			<button disabled>Refuel</button>
-			<button>Refine</button>
-			<button>Mine</button>
-			<button>Survey & Mine</button>
+			{CanRefine && <button>Refine</button>}
+			{MineableLocation && CanMine && (
+				<button onClick={() => Mine()} disabled={isInMineState}>
+					Mine
+				</button>
+			)}
+			{MineableLocation && CanMine && CanSurvery && (
+				<button onClick={SurveyMine} disabled={isInMineState}>
+					Survey & Mine
+				</button>
+			)}
+			{timeLeft.total_seconds > 0 && <span>Cooldown: {timeLeft.total_seconds}s</span>}
 		</div>
 	);
 }
